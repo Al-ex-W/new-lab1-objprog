7884b7638f4d5b09d1cf20da3ad51cd866bbb717
RadmehrDaniel <reza-radmehr@hotmail.com>
2025-02-25 14:58:01 +0100

    redvovisning lab3 uppdagering 2025-02-25

--- src/main/java/CarController.java
+++ src/main/java/CarController.java
@@ -3,14 +3,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-/*
-* This class represents the Controller part in the MVC pattern.
-* It's responsibilities is to listen to the View and responds in a appropriate manner by
-* modifying the model state and the updating the view.
- */
+
 
 public class CarController {
-    // member fields:
 
     // The delay (ms) corresponds to 20 updates a sec (hz)
     private final int delay = 50;
@@ -37,9 +32,6 @@ public class CarController {
         car.startEngine();
     }
 
-    /* Each step the TimerListener moves all the cars in the list and tells the
-    * view to update its images. Change this method to your needs.
-    * */
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             int index = 0;
--- src/main/java/DrawPanel.java
+++ src/main/java/DrawPanel.java
@@ -6,15 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
 
-// This panel represents the animated part of the view with the car images.
+
 
 public class DrawPanel extends JPanel{
 
-//    // Just a single image, TODO: Generalize
-//    BufferedImage volvoImage;
-//    // To keep track of a single car's position
-//    Point volvoPoint = new Point();
-//
+
     BufferedImage volvoWorkshopImage;
     Point volvoWorkshopPoint = new Point(300,300);
     List<Point> carPoints = new ArrayList<>();
@@ -35,12 +31,7 @@ public class DrawPanel extends JPanel{
         this.setBackground(Color.green);
         // Print an error message in case file is not found with a try/catch block
         try {
-            // You can remove the "pics" part if running outside of IntelliJ and
-            // everything is in the same main folder.
-            // volvoImage = ImageIO.read(new File("Volvo240.jpg"));
 
-            // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
-            // if you are starting in IntelliJ.
             carPoints.add(new Point(0, 0));
             carPoints.add(new Point(0, 0));
             carPoints.add(new Point(0, 0));
@@ -56,8 +47,7 @@ public class DrawPanel extends JPanel{
 
     }
 
-    // This method is called each time the panel updates/refreshes/repaints itself
-    // TODO: Change to suit your needs.
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
